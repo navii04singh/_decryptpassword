6b53b4544c811e417e562faa8589765dbe117bf6
Navdeep Singh <58282970+navii04singh@users.noreply.github.com>
2023-12-30 23:00:02 -0700

    Add project files.

---
+++ decryptpwd/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="decryptpwd._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+   <div class="row">
+      <div class="col-md-6 offset-md-3">
+         <div class="form-group">
+            <asp:Label ID="lblDecryptedPassword" runat="server" CssClass="text-danger"></asp:Label>
+            <asp:TextBox ID="txtDecrypt" runat="server" CssClass="form-control" placeholder="Encrypted Password"></asp:TextBox>
+         </div>
+         <div class="form-group">
+            <asp:Button ID="btnDecrypt" runat="server" Text="Decrypt" CssClass="btn btn-primary btn-block" OnClick="btnDecrypt_Click" />
+            <asp:Button ID="btnClearDecrypt" runat="server" Text="Clear" CssClass="btn btn-secondary btn-block" OnClick="btnClearDecrypt_Click" />
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-6 offset-md-3">
+         <div class="form-group">
+            <asp:Label ID="lblEncryptedPassword" runat="server" CssClass="text-success"></asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server" CssClass="form-control" placeholder="Password"></asp:TextBox>
+         </div>
+         <div class="form-group">
+            <asp:Button ID="btnEncrypt" runat="server" Text="Encrypt" CssClass="btn btn-primary btn-block" OnClick="btnEncrypt_Click" />
+         </div>
+      </div>
+   </div>
+</asp:Content>
---
+++ decryptpwd/Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace decryptpwd
+{
+
+    public partial class _Default : Page
+    {
+
+        protected void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string clearText = txtPassword.Text;
+            string encryptedText = encrypt(clearText);
+            ViewState["EncryptedPassword"] = encryptedText;
+            lblEncryptedPassword.Text = encryptedText;
+        }
+        public string encrypt(string clearText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+
+
+
+
+
+        protected void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            string cipherText = txtDecrypt.Text;
+            if (!string.IsNullOrEmpty(cipherText))
+            {
+                string decryptedText = decrypt(cipherText);
+                lblDecryptedPassword.Text = decryptedText;
+            }
+        }
+        protected void btnClearDecrypt_Click(object sender, EventArgs e)
+        {
+            txtDecrypt.Text = string.Empty;
+            lblDecryptedPassword.Text = string.Empty;
+        }
+        public string decrypt(string cipherText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
+}
